7064a0f63264e5c3600bdec923488676790b01a3
aldietri <Alexteske99@gmail.com>
2022-05-15 10:08:45 +0200

    Feature Evaluation and Data Folder Cleaning

--- Helper_Functions/evaluation_pipeline.py
+++ Helper_Functions/evaluation_pipeline.py
@@ -1,10 +1,9 @@
+from re import sub
 import pandas as pd
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from imblearn.over_sampling import SMOTE
 import time
 import warnings
@@ -21,20 +20,17 @@ def compute_points(predictions, gold_standard):
     return points
         
 
-def evaluate_models(X_train, y_train, X_test, y_test):
+def evaluate_models(X_train, y_train, X_test, y_test, score_name):
 
     # Create a list of models
     models = []
     models.append(("LogisticRegression", LogisticRegression()))
-    models.append(("GaussianNB", GaussianNB()))
-    models.append(("KNeighborsClassifier", KNeighborsClassifier()))
-    models.append(("DecisionTreeClassifier", DecisionTreeClassifier()))
-    models.append(("RandomForestClassifier", RandomForestClassifier()))
+    models.append(("XGBoost Classifier", XGBClassifier()))
+    # models.append(("RandomForestClassifier", RandomForestClassifier()))
 
     # A dictionary for all the distinct models 
     model_scores_dict = {"model_name" : [], 
-                     "score"   : [], 
-                     "time"    : []
+                     score_name   : [], 
                     }
 
     #Sampling
@@ -44,14 +40,11 @@ def evaluate_models(X_train, y_train, X_test, y_test):
 
     # Testing all models
     best_score = 0
-    best_labels = []
     with warnings.catch_warnings():
         # Ignore warnings for readability
         warnings.simplefilter("ignore")
 
         for model_name, model in models:
-
-            start = time.time()
 
             model.fit(X_train_sampled, y_train_sampled)
 
@@ -65,12 +58,68 @@ def evaluate_models(X_train, y_train, X_test, y_test):
                 best_labels = labels
 
             model_scores_dict["model_name"].append(model_name)
-            model_scores_dict["score"].append(score)
-            model_scores_dict["time"].append(time.time() - start)
+            model_scores_dict[score_name].append(score)
         
         model_score_df = pd.DataFrame(model_scores_dict).set_index("model_name")
-        model_score_df.sort_values(by=["score"], ascending=False)
-        display(model_score_df)
+        model_score_df.sort_values(by=[score_name], ascending=False)
 
-    return best_labels
+    return model_score_df, best_labels
 
+def get_test_data(X_train, month,  path_to_data):
+    test = pd.read_csv(f"{path_to_data}/test_{month}.csv", sep="|")
+    test = pd.merge(test, X_train, on=["userID", "itemID"], how="left").drop_duplicates(["userID", "itemID"])
+
+    test_target = test["prediction"]
+    test_body = test.drop(columns="prediction")
+
+    return test_body, test_target
+
+def get_submission_csv(pred_dec, pred_jan, path_to_data):
+    submission_dec = pd.read_csv(f"{path_to_data}/test_dec.csv", sep="|")
+    submission_dec["prediction"] = pred_dec
+
+    submission_jan = pd.read_csv(f"{path_to_data}/test_jan.csv", sep="|")
+    submission_jan["prediction"] = pred_jan
+
+    return submission_dec, submission_jan
+
+def calculate_feature_gain(X_train_dec, y_train_dec, X_train_jan, y_train_jan, features_to_test, path_to_data):
+
+    X_test_dec, y_test_dec = get_test_data(X_train_dec, "dec", path_to_data)
+    
+    base_features = ["userID", "itemID", "order", "brand", "feature_1", "feature_2", "feature_3", "feature_4", "feature_5"]
+
+    features = base_features + features_to_test
+
+    df_dec_without_features, labels_dec_without_features = evaluate_models(X_train_dec[base_features], y_train_dec, X_test_dec[base_features], y_test_dec, "w/o features dec")
+    df_dec_with_features, labels_dec_with_features = evaluate_models(X_train_dec[features], y_train_dec, X_test_dec[features], y_test_dec, "with features dec")
+
+    df_dec = pd.merge(df_dec_without_features, df_dec_with_features, on=["model_name"])
+    df_dec["gain dec"] = df_dec["with features dec"] - df_dec["w/o features dec"]
+
+    X_test_jan, y_test_jan = get_test_data(X_train_jan, "jan", path_to_data)
+
+    df_jan_without_features, labels_jan_without_features = evaluate_models(X_train_jan[base_features], y_train_jan, X_test_jan[base_features], y_test_jan, "w/o features jan")
+    df_jan_with_features, labels_jan_with_features = evaluate_models(X_train_jan[features], y_train_jan, X_test_jan[features], y_test_jan, "with features jan")
+
+    df_jan = pd.merge(df_jan_without_features, df_jan_with_features, on=["model_name"])
+    df_jan["gain jan"] = df_jan["with features jan"] - df_jan["w/o features jan"]
+
+    df = pd.merge(df_dec, df_jan, on=["model_name"])
+    df["overall score w/o features"] = (df["w/o features dec"] + df["w/o features jan"]) / 2
+    df["overall score with features"] = (df["with features dec"] + df["with features jan"]) / 2
+    df["overall gain"] = df["overall score with features"] - df["overall score w/o features"]
+
+    if df_dec_without_features.max().max() > df_dec_with_features.max().max():
+        best_labels_dec = labels_dec_without_features
+    else: 
+        best_labels_dec = labels_dec_with_features
+
+    if df_jan_without_features.max().max() > df_jan_with_features.max().max():
+        best_labels_jan = labels_jan_without_features
+    else: 
+        best_labels_jan = labels_jan_with_features
+
+    submission_dec, submission_jan = get_submission_csv(best_labels_dec, best_labels_jan, path_to_data)
+
+    return df, submission_dec, submission_jan
